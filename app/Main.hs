cd154f66f194e3e28a405f8b04347f453a40b017
lim6112j <bclim@thecheat.co.kr>
2021-08-10 15:38:36 +0900

    send image raw data still failed

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,13 @@ module Main where
 import Lib
 import Mysql
 import Database.MySQL.Base as DMB
-import Data.ByteString.Lazy.UTF8 as BLU hiding (length, putStrLn, concatMap)
+import qualified Data.ByteString.Lazy.UTF8 as BLU hiding (ByteStribg,length, putStrLn, concatMap)
+import qualified Data.ByteString as B (ByteString,readFile, concat, putStr, concatMap)
+funC :: [FilePath] -> IO ([B.ByteString])
+funC names= mapM f names
+  where f x = do
+          byteStr <- B.readFile x
+          return (B.concat ["INSERT INTO uploaded_images (imageData) VALUES('", byteStr, "')\n"])
 main :: IO ()
 main = do
   putStrLn "type filepath you want"
@@ -14,7 +20,12 @@ main = do
   putStrLn $ "Items to be commited to Database : " ++ show (length names)
   putStrLn "Type any to continue or Stop with C-c"
   temp <- getLine
-  let qStr = concatMap (\x -> "INSERT INTO uploaded_images(imageType, imageData) VALUES('N/A', LOAD_FILE('" ++  x ++ "'))\n") names
-  putStrLn qStr
-  insertData $ Query . BLU.fromString $ qStr
+  fileContents <- funC names
+  B.putStr $ B.concat (take 100 fileContents)
+  --let flatFileContents = B.concat fileContents
+  --B.putStr $ B.concat fileContents
+  --putStrLn $ foldl (++) "" fileContents
+  --let qStr = map (\x -> "INSERT INTO uploaded_images (imageData) VALUES('" ++ x ++ "')\n") fileContents
+  --putStrLn qStr
+  --insertData $ Query . BLU.fromString $ (show qStr)
   getDBInfo
