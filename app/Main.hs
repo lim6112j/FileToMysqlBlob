fbdb293e1b107d730374c4a711dd80e2d13577eb
lim6112j <lim6112j@gmail.com>
2021-08-10 23:22:50 +0900

    db insert check successful

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,27 @@
 {-# OPTIONS_GHC -Wno-deferred-out-of-scope-variables #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -XDataKinds#-}
 module Main where
 
 import Lib
 import Mysql
 import Database.MySQL.Base as DMB
-import qualified Data.ByteString.Lazy.UTF8 as BLU hiding (ByteStribg,length, putStrLn, concatMap)
-import qualified Data.ByteString as B (ByteString,readFile, concat, putStr, concatMap)
-funC :: [FilePath] -> IO ([Query])
-funC names= mapM f names
+import qualified Data.ByteString as B
+readPath :: [FilePath] -> IO ([B.ByteString])
+readPath = mapM f
   where f x = do
           byteStr <- B.readFile x
-          let querystr = "INSERT INTO uploaded_images (imageData) VALUES('?')"
-              query = DMB.renderParams querystr $ DMB.render $ One (MySQLBytes byteStr)
-          return query
+          return byteStr
 main :: IO ()
 main = do
   putStrLn "type filepath you want"
   path <- getLine
-  names <- getRecursiveContents path
-  putStrLn $ "Items to be commited to Database : " ++ show (length names)
+  paths <- getRecursiveContents path
+  putStrLn $ "Items to be commited to Database : " ++ show (length paths)
   putStrLn "Type any to continue or Stop with C-c"
-  temp <- getLine
-  queries <- funC names
+  --byteStrs <- readPath paths
+  --putStrLn $ show byteStrs
+  insertData
   --B.putStr $ B.concat (take 100 queries)
   --let flatFileContents = B.concat fileContents
   --B.putStr $ B.concat fileContents
--- src/Mysql.hs
+++ src/Mysql.hs
@@ -3,13 +3,14 @@
 module Mysql(getDBInfo, insertData) where
 import Database.MySQL.Base
 import qualified System.IO.Streams as Streams
+import qualified Data.ByteString as B
 getDBInfo :: IO()
 getDBInfo = do
   conn <- connect
     defaultConnectInfo {ciHost="133.186.212.161", ciPort=3306, ciUser = "root", ciPassword = "thecheat99))", ciDatabase = "thecheat_api"}
   (defs, is) <- query_ conn "SELECT * FROM uploaded_images limit 1"
   print =<< Streams.toList is
-insertData (qry::Query) = do
+insertData  = do
   conn <- connect
     defaultConnectInfo {ciHost="133.186.212.161", ciPort=3306, ciUser = "root", ciPassword = "thecheat99))", ciDatabase = "thecheat_api"}
-  withTransaction conn $ executeMany_ conn qry
+  execute conn "INSERT INTO uploaded_images(imageData) VALUES(?)" [MySQLBytes "123123123"]
