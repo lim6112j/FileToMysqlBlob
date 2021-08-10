244bd1a79e46bd622ac52839e020a2cdeef293f6
lim6112j <bclim@thecheat.co.kr>
2021-08-10 13:11:42 +0900

    mysql load-file method

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,11 @@
 {-# OPTIONS_GHC -Wno-deferred-out-of-scope-variables #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
 import Mysql
+import Database.MySQL.Base as DMB
+import Data.ByteString.Lazy.UTF8 as BLU hiding (length, putStrLn, concatMap)
 main :: IO ()
 main = do
   putStrLn "type filepath you want"
@@ -11,5 +14,7 @@ main = do
   putStrLn $ "Items to be commited to Database : " ++ show (length names)
   putStrLn "Type any to continue or Stop with C-c"
   temp <- getLine
-  putStrLn $ concatMap (\x -> "INSERT INTO db (id, imageBlob) VALUES(1, LOAD_FILE('" ++  x ++ "'))\n") names
+  let qStr = concatMap (\x -> "INSERT INTO uploaded_images(imageType, imageData) VALUES('N/A', LOAD_FILE('" ++  x ++ "'))\n") names
+  putStrLn qStr
+  insertData $ Query . BLU.fromString $ qStr
   getDBInfo
--- src/Mysql.hs
+++ src/Mysql.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
-
-module Mysql(getDBInfo) where
+{-# LANGUAGE ScopedTypeVariables #-}
+module Mysql(getDBInfo, insertData) where
 import Database.MySQL.Base
 import qualified System.IO.Streams as Streams
 getDBInfo :: IO()
 getDBInfo = do
   conn <- connect
-    defaultConnectInfo {ciHost="127.0.0.1", ciPort=3306, ciUser = "root", ciPassword = "root", ciDatabase = "mysql"}
-  (defs, is) <- query_ conn "SELECT * FROM db"
+    defaultConnectInfo {ciHost="133.186.212.161", ciPort=3306, ciUser = "root", ciPassword = "thecheat99))", ciDatabase = "thecheat_api"}
+  (defs, is) <- query_ conn "SELECT * FROM uploaded_images limit 1"
   print =<< Streams.toList is
+insertData (qry::Query) = do
+  conn <- connect
+    defaultConnectInfo {ciHost="133.186.212.161", ciPort=3306, ciUser = "root", ciPassword = "thecheat99))", ciDatabase = "thecheat_api"}
+  withTransaction conn $ executeMany_ conn qry
