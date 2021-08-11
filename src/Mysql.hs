0d00d9bd100c45af9283c8482c182d843b5d0656
lim6112j <lim6112j@gmail.com>
2021-08-12 01:26:59 +0900

    code action

--- src/Mysql.hs
+++ src/Mysql.hs
@@ -21,11 +21,11 @@ getDBInfo = do
     defaultConnectInfo {ciHost="133.186.212.161", ciPort=3306, ciUser = "root", ciPassword = "thecheat99))", ciDatabase = "thecheat_api"}
   (defs, is) <- query_ conn "SELECT * FROM uploaded_images limit 1"
   print =<< Streams.toList is
-insertData (paths::[(String,B.ByteString)]) = do
-
+insertData :: [(String, B.ByteString)] -> IO String
+insertData paths = do
   conn <- connect
     defaultConnectInfo {ciHost="133.186.212.161", ciPort=3306, ciUser = "root", ciPassword = "thecheat99))", ciDatabase = "thecheat_api"}
-  let params =map (\(x::String, y::B.ByteString) -> [MySQLText (T.pack x), MySQLBytes y]) paths
+  let params = map (\(x, y) -> [MySQLText (T.pack x), MySQLBytes y]) paths
   print $ length params
   oks <- sequence $ concat $ executeMulti conn params
   --oks <- withTransaction conn $ executeMany conn "INSERT INTO uploaded_images(imageData) VALUES(?)" params
